837c74330ee652813eba2f928cbbdc86471739c3
Suhjung Park <145967352+Imggaggu@users.noreply.github.com>
2025-07-15 02:47:17 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int yr;
+    cin >> yr;
+    if ((yr%4==0)&&(yr%100!=0)){
+        cout << 1;
+    }else if(yr%400==0){
+        cout << 1;
+    }else{
+        cout << 0;
+    }
+    return 0;
+}
