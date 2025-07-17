68484f2b0d32f38eaea19b091ca11b42ffee22f0
Imggaggu <145967352+Imggaggu@users.noreply.github.com>
2025-07-17 23:15:53 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1475. 방 번호/방 번호.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    int arr[11]={0};
+    while(N>0){
+        int idx=N%10;
+        arr[idx]++;
+        N=N/10;
+    }
+    if ((arr[9]+arr[6])%2==0){
+        arr[10]=(arr[9]+arr[6])/2;
+        arr[6]=0;
+        arr[9]=0;
+    }else{
+        arr[10]=(arr[9]+arr[6]+1)/2;
+        arr[6]=0;
+        arr[9]=0;
+    }
+    int max=0;
+    for (int i=0; i<11; i++){
+        if(max<arr[i]){
+            max=arr[i];
+        }
+    }
+    cout << max;
+    return 0;
+}
